150be6d72f3e930c1b2dbc7e3ddab48f94cf877d
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-03-20 22:59:20 -0500

    hoisting and debug

---
+++ src/debug/index.js
@@ -0,0 +1,9 @@
+var a = "Hello";
+
+function hello() {
+  let b = "Hello world";
+  let c = "Hello World XD";
+  debugger;
+}
+
+hello();
---
+++ src/hoisting/index.js
@@ -0,0 +1,11 @@
+// console.log(nameOfDog);// undefined
+// var nameOfDog = "Elmo"; 
+
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`the best dog is ${dog}`);
+}
+
+var dog = "Elmo";
+// the best dog is undefined
